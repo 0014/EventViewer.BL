e4f48060c8e8838dcde4da24489831d7d6a922cc
gencoa <Arif.Gencosmanoglu@astrodynetdi.com>
2020-01-28 13:53:30 -0500

    Removed the existing migrations

--- EventViewer.Data/EventType.cs
+++ EventViewer.Data/EventType.cs
@@ -1,9 +1,13 @@
-﻿namespace EventViewer.Data
+﻿using System.Collections.Generic;
+
+namespace EventViewer.Data
 {
     public class EventType
     {
         public int EventTypeId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+
+        public ICollection<Event> Events { get; set; }
     }
 }
